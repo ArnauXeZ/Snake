5fc78c9b9705b810d214f26a6732ad410d619208
ArnauXeZ <arnauetxpop@get.cat>
2023-10-09 13:47:37 +0200

    v2

--- Assets/Script/Snake.cs
+++ Assets/Script/Snake.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,15 +15,29 @@ public class Snake : MonoBehaviour
     private float gridMoveTimer;
     private float gridMoveTimerMax = 1f; // La serpiente se moverá a cada segundo
 
+    private LevelGrid levelGrid;
+
     private void Awake()
     {
         startGridPosition = new Vector2Int(0, 0);
         gridPosition = startGridPosition;
 
         gridMoveDirection = new Vector2Int(0, 1); // Dirección arriba por defecto
+        transform.eulerAngles = Vector3.zero; // Rotación arriba por defecto
     }
 
     private void Update()
+    {
+        HandleMoveDirection();
+        HandleGridMovement();
+    }
+
+    public void Setup(LevelGrid levelGrid)
+    {
+        this.levelGrid = levelGrid;
+    }
+
+    private void HandleGridMovement()
     {
         gridMoveTimer += Time.deltaTime;
         if (gridMoveTimer >= gridMoveTimerMax)
@@ -31,6 +46,67 @@ public class Snake : MonoBehaviour
             gridMoveTimer -= gridMoveTimerMax;
 
             transform.position = new Vector3(gridPosition.x, gridPosition.y, 0);
+            transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(gridMoveDirection));
         }
+    }
+
+    private void HandleMoveDirection()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Cambio dirección hacia arriba
+        if (verticalInput > 0) // Si he pulsado hacia arriba (W o Flecha Arriba)
+        {
+            if (gridMoveDirection.x != 0) // Si iba en horizontal
+            {
+                // Cambio la dirección hacia arriba (0,1)
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = 1;
+            }
+        }
+
+        // Cambio dirección hacia abajo
+        // Input es abajo?
+        if (verticalInput < 0)
+        {
+            // Mi dirección hasta ahora era horizontal
+            if (gridMoveDirection.x != 0)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
+        }
+
+        // Cambio dirección hacia derecha
+        if (horizontalInput > 0)
+        {
+            if (gridMoveDirection.y != 0)
+            {
+                gridMoveDirection.x = 1;
+                gridMoveDirection.y = 0;
+            }
+        }
+
+        // Cambio dirección hacia izquierda
+        if (horizontalInput < 0)
+        {
+            if (gridMoveDirection.y != 0)
+            {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+        }
+    }
+
+    private float GetAngleFromVector(Vector2Int direction)
+    {
+        float degrees = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (degrees < 0)
+        {
+            degrees += 360;
+        }
+
+        return degrees - 90;
     }
 }
